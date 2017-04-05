1362ea6ae93772fc9568a7f3476650001e463553
Carlos A. Vaca Morales <carlosv@rain.agency>
2017-04-05 15:31:35 -0600

    using yeoman test modules

--- test/app.js
+++ test/app.js
@@ -1,1 +1,16 @@
 'use strict';
+const Generator = require('../generators/app/index'),
+	helpers = require('yeoman-test'),
+	assert = require('yeoman-assert'),
+	path = require('path');
+
+describe('Generator', () => {
+	beforeEach(function () {
+	  // The object returned act like a promise, so return it to wait until the process is done
+	  return helpers.run(path.join(__dirname, '../generators/app'))
+	    .withOptions({ foo: 'bar' })    // Mock options passed in
+	    .withArguments(['name'])      // Mock the arguments
+	    .withPrompts({ name: 'name', author: 'author', subDirConfim: false }); // Mock the prompt answers
+	});
+
+});
