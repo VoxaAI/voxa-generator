3231dc9b8f7aaa941856414e5ad34fcf50cda6fa
Rommel Berrios <rommelb@rain.agency>
2017-04-21 16:57:20 -0600

    Improve config to be easy readable closes mediarain/generator-voxa-skill#3

--- generators/app/templates/config/env.js
+++ generators/app/templates/config/env.js
@@ -1,17 +1,27 @@
 'use strict';
 
+const _ = require('lodash');
+
 function getEnv() {
-  if (process.env.NODE_ENV) {
-    return process.env.NODE_ENV;
-  }
-  if (process.env.AWS_LAMBDA_FUNCTION_NAME) {
-    // TODO put your own lambda function name here
-    if (process.env.AWS_LAMBDA_FUNCTION_NAME === '') {
-      return 'production';
-    }
-    return 'staging';
+  const NODE_ENV = process.env.NODE_ENV;
+  const AWS_LAMBDA_FUNCTION_NAME = process.env.AWS_LAMBDA_FUNCTION_NAME;
+
+  // Environment base on lambda function name
+  // TODO put your own lambda function name here
+
+  const AWS_LAMBDA_PRODUCTION_ENV = ['LAMBDA_FUNCTION_NAME_PROD']; // production
+  const AWS_LAMBDA_STAGING_ENV = ['LAMBDA_FUNCTION_NAME_STAGE'];  // staging
+
+  if (NODE_ENV) {
+    return NODE_ENV;
   }
 
+  if (AWS_LAMBDA_FUNCTION_NAME) {
+    if (_.includes(AWS_LAMBDA_PRODUCTION_ENV, AWS_LAMBDA_FUNCTION_NAME)) return 'production';
+    if (_.includes(AWS_LAMBDA_STAGING_ENV, AWS_LAMBDA_FUNCTION_NAME)) return 'staging';
+  }
+
+  // Default local
   return 'local';
 }
 
