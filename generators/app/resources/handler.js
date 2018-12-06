2032dda00e90b2ec9a7863484bd41e89303ae2b9
Christian Torres <chtorrez@gmail.com>
2018-12-05 21:39:11 -0600

    Fixing issue with operator ...

--- generators/app/resources/handler.js
+++ generators/app/resources/handler.js
@@ -1,60 +1,60 @@
-  const _ = require('lodash');
-  let
-    questions = require('./prompts'),
-    initialPrompt = questions.initialPrompt,
-    secondPrompt = questions.secondPrompt,
-    pluginsPrompt = questions.pluginsPrompt,
-    installPrompt = questions.installPrompt,
-    _plugins = require('./plugins');
-  
-  function requiresToString(requiresObject) {
-    return _.reduce(requiresObject, (str, value, key) => {
-      return `${str}\nconst ${key} = require('${value}');`;
-    }, '');
+const _ = require('lodash');
+let
+  questions = require('./prompts'),
+  initialPrompt = questions.initialPrompt,
+  secondPrompt = questions.secondPrompt,
+  pluginsPrompt = questions.pluginsPrompt,
+  installPrompt = questions.installPrompt,
+  _plugins = require('./plugins');
+
+function requiresToString(requiresObject) {
+  return _.reduce(requiresObject, (str, value, key) => {
+    return `${str}\nconst ${key} = require('${value}');`;
+  }, '');
+}
+
+module.exports = class PromptHandler {
+
+  initialPrompt() {
+    _.set(initialPrompt[0], 'default', this.appname);
+    return this.prompt(initialPrompt);
   }
 
-  module.exports = class PromptHandler {
-
-    initialPrompt() {
-      _.set(initialPrompt[0],'default', this.appname);
-      return this.prompt(initialPrompt);
+  secondPrompt(answers) {
+    _.set(secondPrompt[0], 'default', answers.name);
+    if (answers.subDirConfim) {
+      return this.prompt(secondPrompt)
+        .then((res) => {
+          _.set(answers, 'subDir', res.subDir);
+          return answers;
+        });
     }
+    return answers;
+  }
 
-    secondPrompt(answers) {
-        _.set(secondPrompt[0],'default', answers.name);
-        if(answers.subDirConfim){
-            return this.prompt(secondPrompt)
-              .then((res)=>{
-                _.set(answers,'subDir', res.subDir);
-                return answers;
-              });
-        }
-        return answers;
-    }
-
-    thirdPrompt(answers) {
-      return this.prompt(installPrompt)
+  thirdPrompt(answers) {
+    return this.prompt(installPrompt)
       .then((res) => {
-        _.set(answers,'install', res);
+        _.set(answers, 'install', res);
         return answers;
       });
-    }
+  }
 
-    setPlugins(answers) {
-      return this.prompt(pluginsPrompt)
-      .then((res) =>{
-        _.set(answers,'plugins', res);
+  setPlugins(answers) {
+    return this.prompt(pluginsPrompt)
+      .then((res) => {
+        _.set(answers, 'plugins', res);
         return answers;
       });
-    }
+  }
 
-    finishPrompts(answers) {
-      const { language } = answers.install;
-      this.language = language.toLowerCase();
+  finishPrompts(answers) {
+    const { language } = answers.install;
+    this.language = language.toLowerCase();
 
-      const plugins = answers.plugins,
-      path =  this.templatePath(`${this.language}/src/app/_main.js`),
-      newPath =  this.templatePath(`${this.language}/src/app/main.js`),
+    const plugins = answers.plugins,
+      path = this.templatePath(`${this.language}/src/app/_main.js`),
+      newPath = this.templatePath(`${this.language}/src/app/main.js`),
 
       configPath = this.templatePath(`${this.language}/src/config/_index.js`),
       configNewPath = this.templatePath(`${this.language}/src/config/index.js`),
@@ -70,25 +70,25 @@
       prodNewPath = this.templatePath(`common/config/production.json`),
       envFile = JSON.parse(this.fs.read(envPath)),
 
-      filerBuffer   = this.fs.read(path),
+      filerBuffer = this.fs.read(path),
       requiresHook = '/*******  requires  *******/',
-      hook  = '/*******  plugins  *******/',
+      hook = '/*******  plugins  *******/',
 
       arrFiles = [],
 
       dir = answers.subDirConfim ? answers.subDir + '/' : '';
 
-      let newRequires = {},
-        newBuffer = '',
-        configRequires = {},
-        configBuffer = '';
+    let newRequires = {},
+      newBuffer = '',
+      configRequires = {},
+      configBuffer = '';
 
-      _.set(answers,'dir', dir);
+    _.set(answers, 'dir', dir);
 
-      if(_.keys(answers).length > 0){
-        _.forIn(plugins, (val, key) =>{
-          if(val){
-            let i = _.findIndex(_plugins,{name: key} ), 
+    if (_.keys(answers).length > 0) {
+      _.forIn(plugins, (val, key) => {
+        if (val) {
+          let i = _.findIndex(_plugins, { name: key }),
             requires = _plugins[i].requires,
             usage = _plugins[i].usage,
             dependencies = _plugins[i].dependencies,
@@ -96,174 +96,174 @@
             env = _plugins[i].env,
             config = _plugins[i].config;
 
-            if(requires) {
-              newRequires = { ...(newRequires || {}), ...requires };
-            }
-
-            if(config) {
-              configRequires = { ...(configRequires || {}), ...config.requires };
-              configBuffer += config.usage + '\n\n';
-            }
-
-            if(dependencies) {
-              _.forEach(dependencies, function(item){
-                _.set(jsonFile,`dependencies.${item.name}`,item.version); 
-              });
-            }
-
-            if(files){
-              _.forEach(files, (item) =>{
-                arrFiles.push(item);
-              });
-            }
-
-            if(env){
-              _.forEach(env, (item) =>{
-                _.mapKeys(item, (value, key) => {
-                  _.set(envFile, key, value);  
-                });
-              });
-            }
-
-            //inserting usage
-            newBuffer += usage + '\n\n';
+          if (requires) {
+            newRequires = Object.assign({}, newRequires || {}, requires);
           }
-        });
 
-        //creating main
-        newBuffer = filerBuffer.replace(hook, hook + '\n' + newBuffer.trimRight());
-        newBuffer = newBuffer.replace(requiresHook, requiresHook + '\n' + requiresToString(newRequires).trim());
-        this.fs.write(newPath, newBuffer);
+          if (config) {
+            configRequires = Object.assign({}, configRequires || {}, config.requires);
+            configBuffer += config.usage + '\n\n';
+          }
 
-        //creating config/index.js
-        configBuffer = configFile.replace(hook, hook + '\n' + configBuffer.trimRight());
-        configBuffer = configBuffer.replace(requiresHook, requiresHook + '\n' + requiresToString(configRequires).trimRight());
-        this.fs.write(configNewPath, configBuffer);
+          if (dependencies) {
+            _.forEach(dependencies, function (item) {
+              _.set(jsonFile, `dependencies.${item.name}`, item.version);
+            });
+          }
 
-        //creating package.json
-        this.fs.write(jsonNewPath,JSON.stringify(jsonFile, null, '\t'));
-      }
+          if (files) {
+            _.forEach(files, (item) => {
+              arrFiles.push(item);
+            });
+          }
 
-      if(arrFiles.length > 0) {
-        _.set(answers,'services', arrFiles);  
-      }
+          if (env) {
+            _.forEach(env, (item) => {
+              _.mapKeys(item, (value, key) => {
+                _.set(envFile, key, value);
+              });
+            });
+          }
 
-      _.set(answers,'env', envFile);  
-      this.fs.write(envNewPath, JSON.stringify(envFile, null, '\t'));
-      this.fs.write(stagNewPath, JSON.stringify(envFile, null, '\t'));
-      this.fs.write(prodNewPath, JSON.stringify(envFile, null, '\t'));
-
-      this.props = answers;
-      return this.props;
-    }
-
-    setNewDestinationRoot(newRoute) {
-      this.destinationRoot(newRoute);
-    }
-
-    creatingConfigFiles() {
-      this.fs.copy(
-        this.templatePath(`${this.language}/.editorconfig`),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath(`${this.language}/.eslintrc.json`),
-        this.destinationPath('.eslintrc.json')
-      );
-      this.fs.copy(
-        this.templatePath('common/_gitignore'),
-        this.destinationPath('.gitignore')
-      );
-      this.fs.copy(
-        this.templatePath(`${this.language}/.nvmrc`),
-        this.destinationPath('.nvmrc')
-      );
-      this.fs.copyTpl(
-        this.templatePath(`${this.language}/package.json`),
-        this.destinationPath('package.json'), {
-          name: this.props.name,
-          author: this.props.author
+          //inserting usage
+          newBuffer += usage + '\n\n';
         }
-      );
-      this.fs.copyTpl(
-        this.templatePath(`${this.language}/README.md`),
-        this.destinationPath('README.md'), {
-          name: this.props.name
-        }
-      );
-      this.fs.copy(
-        this.templatePath(`${this.language}/server.js`),
-        this.destinationPath('server.js')
-      );
-      this.fs.copy(
-        this.templatePath(`${this.language}/serverless.yml`),
-        this.destinationPath('serverless.yml')
-      );
+      });
+
+      //creating main
+      newBuffer = filerBuffer.replace(hook, hook + '\n' + newBuffer.trimRight());
+      newBuffer = newBuffer.replace(requiresHook, requiresHook + '\n' + requiresToString(newRequires).trim());
+      this.fs.write(newPath, newBuffer);
+
+      //creating config/index.js
+      configBuffer = configFile.replace(hook, hook + '\n' + configBuffer.trimRight());
+      configBuffer = configBuffer.replace(requiresHook, requiresHook + '\n' + requiresToString(configRequires).trimRight());
+      this.fs.write(configNewPath, configBuffer);
+
+      //creating package.json
+      this.fs.write(jsonNewPath, JSON.stringify(jsonFile, null, '\t'));
     }
 
-    creatingAppFiles() {
-      this.fs.copy(
-        this.templatePath('common/config'),
-        this.destinationPath('src/config')
-      );
+    if (arrFiles.length > 0) {
+      _.set(answers, 'services', arrFiles);
+    }
 
-      this.fs.copy(
-        this.templatePath(`${this.language}/src/config/index.js`),
-        this.destinationPath('src/config/index.js')
-      );
+    _.set(answers, 'env', envFile);
+    this.fs.write(envNewPath, JSON.stringify(envFile, null, '\t'));
+    this.fs.write(stagNewPath, JSON.stringify(envFile, null, '\t'));
+    this.fs.write(prodNewPath, JSON.stringify(envFile, null, '\t'));
 
-      this.fs.copy(
-        this.templatePath(`${this.language}/src/config/env.js`),
-        this.destinationPath('src/config/env.js')
-      );
+    this.props = answers;
+    return this.props;
+  }
 
-      if(this.props.services) {
-        _.forEach(this.props.services, (item) =>{
-          this.fs.copy(
-            this.templatePath(`${this.language}/src/${item}`),
-            this.destinationPath(`src/${item}`)
-          );
-        });
+  setNewDestinationRoot(newRoute) {
+    this.destinationRoot(newRoute);
+  }
+
+  creatingConfigFiles() {
+    this.fs.copy(
+      this.templatePath(`${this.language}/.editorconfig`),
+      this.destinationPath('.editorconfig')
+    );
+    this.fs.copy(
+      this.templatePath(`${this.language}/.eslintrc.json`),
+      this.destinationPath('.eslintrc.json')
+    );
+    this.fs.copy(
+      this.templatePath('common/_gitignore'),
+      this.destinationPath('.gitignore')
+    );
+    this.fs.copy(
+      this.templatePath(`${this.language}/.nvmrc`),
+      this.destinationPath('.nvmrc')
+    );
+    this.fs.copyTpl(
+      this.templatePath(`${this.language}/package.json`),
+      this.destinationPath('package.json'), {
+        name: this.props.name,
+        author: this.props.author
       }
-      else{
+    );
+    this.fs.copyTpl(
+      this.templatePath(`${this.language}/README.md`),
+      this.destinationPath('README.md'), {
+        name: this.props.name
+      }
+    );
+    this.fs.copy(
+      this.templatePath(`${this.language}/server.js`),
+      this.destinationPath('server.js')
+    );
+    this.fs.copy(
+      this.templatePath(`${this.language}/serverless.yml`),
+      this.destinationPath('serverless.yml')
+    );
+  }
+
+  creatingAppFiles() {
+    this.fs.copy(
+      this.templatePath('common/config'),
+      this.destinationPath('src/config')
+    );
+
+    this.fs.copy(
+      this.templatePath(`${this.language}/src/config/index.js`),
+      this.destinationPath('src/config/index.js')
+    );
+
+    this.fs.copy(
+      this.templatePath(`${this.language}/src/config/env.js`),
+      this.destinationPath('src/config/env.js')
+    );
+
+    if (this.props.services) {
+      _.forEach(this.props.services, (item) => {
         this.fs.copy(
-          this.templatePath(`${this.language}/src/services/.gitkeep`),
-          this.destinationPath('src/services/.gitkeep')
-        );  
-      }
-
+          this.templatePath(`${this.language}/src/${item}`),
+          this.destinationPath(`src/${item}`)
+        );
+      });
+    }
+    else {
       this.fs.copy(
-        this.templatePath(`${this.language}/src/app/`),
-        this.destinationPath('src/app/')
-      );
-      this.fs.copy(
-        this.templatePath('common/speech-assets'),
-        this.destinationPath('speech-assets')
-      );
-      this.fs.copy(
-        this.templatePath(`${this.language}/test`),
-        this.destinationPath('test')
+        this.templatePath(`${this.language}/src/services/.gitkeep`),
+        this.destinationPath('src/services/.gitkeep')
       );
     }
 
-    removingUnnecessaryFiles(){
-      //config files
-      this.fs.delete(this.templatePath(`${this.language}/package.json`));
+    this.fs.copy(
+      this.templatePath(`${this.language}/src/app/`),
+      this.destinationPath('src/app/')
+    );
+    this.fs.copy(
+      this.templatePath('common/speech-assets'),
+      this.destinationPath('speech-assets')
+    );
+    this.fs.copy(
+      this.templatePath(`${this.language}/test`),
+      this.destinationPath('test')
+    );
+  }
 
-      //app files
-      this.fs.delete(this.templatePath(`${this.language}/src/app/main.js`));
-      this.fs.delete(this.destinationPath(`src/app/_main.js`));
+  removingUnnecessaryFiles() {
+    //config files
+    this.fs.delete(this.templatePath(`${this.language}/package.json`));
 
-      this.fs.delete(this.templatePath(`common/config/local.json`));
-      this.fs.delete(this.destinationPath('src/config/local.json.example'));
+    //app files
+    this.fs.delete(this.templatePath(`${this.language}/src/app/main.js`));
+    this.fs.delete(this.destinationPath(`src/app/_main.js`));
 
-      this.fs.delete(this.templatePath('common/config/production.json'));
-      this.fs.delete(this.destinationPath('src/config/production.json.example'));
+    this.fs.delete(this.templatePath(`common/config/local.json`));
+    this.fs.delete(this.destinationPath('src/config/local.json.example'));
 
-      this.fs.delete(this.templatePath('common/config/staging.json'));
-      this.fs.delete(this.destinationPath('src/config/staging.json.example'));
+    this.fs.delete(this.templatePath('common/config/production.json'));
+    this.fs.delete(this.destinationPath('src/config/production.json.example'));
 
-      this.fs.delete(this.templatePath(`${this.language}/src/config/index.js`));
-    }
+    this.fs.delete(this.templatePath('common/config/staging.json'));
+    this.fs.delete(this.destinationPath('src/config/staging.json.example'));
 
-  };
+    this.fs.delete(this.templatePath(`${this.language}/src/config/index.js`));
+  }
+
+};
