14f2b5f0e2ce42e04aab585f7a359d4f3dd7b24e
Christian Torres <chtorrez@gmail.com>
2018-12-13 15:50:26 -0600

    Linting with prettier.

--- generators/app/index.js
+++ generators/app/index.js
@@ -8,31 +8,32 @@ module.exports = Generator.extend({
   // Configurations will be loaded here.
   // Ask for user input
   prompting() {
-    return handler.initialPrompt.call(this)
-    .then(answers => handler.secondPrompt.call(this, answers))
-    .then(answers => handler.setPlugins.call(this, answers))
-    .then(answers => handler.thirdPrompt.call(this, answers))
-    .then(answers => handler.finishPrompts.call(this,answers))
-    .then(answers => handler.setNewDestinationRoot.call(this,answers.dir));
+    return handler.initialPrompt
+      .call(this)
+      .then(answers => handler.secondPrompt.call(this, answers))
+      .then(answers => handler.setPlugins.call(this, answers))
+      .then(answers => handler.thirdPrompt.call(this, answers))
+      .then(answers => handler.finishPrompts.call(this, answers))
+      .then(answers => handler.setNewDestinationRoot.call(this, answers.dir));
   },
 
   // Writing Logic here
   writing: {
     // Copy the configuration files
-    config: function () { 
+    config: function() {
       handler.creatingConfigFiles.call(this);
     },
 
     // Copy application files
-    app: function () {
+    app: function() {
       handler.creatingAppFiles.call(this);
       handler.removingUnnecessaryFiles.call(this);
     },
 
     // Install Dependencies if wanted
-    install: function () {
-      if(this.props.dependencies){
-        this.installDependencies();  
+    install: function() {
+      if (this.props.dependencies) {
+        this.installDependencies();
       }
     }
   }
