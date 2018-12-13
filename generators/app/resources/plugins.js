dc0ca57567f42062dd77239f076abd51c141180a
Christian Torres <chtorrez@gmail.com>
2018-12-13 16:10:53 -0600

    More linting with prettier.

--- generators/app/resources/plugins.js
+++ generators/app/resources/plugins.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict";
 
 module.exports = [
   {
-    name: 'autoload',
+    name: "autoload",
     dependencies: [
       {
-        name: 'aws-sdk',
-        version: '^2.7.23'
+        name: "aws-sdk",
+        version: "^2.7.23"
       },
       {
-        name: 'dynamodb-doc',
-        version: '^1.0.0'
+        name: "dynamodb-doc",
+        version: "^1.0.0"
       }
     ],
     config: {
       requires: {
-        AWS: 'aws-sdk',
-        https: 'https',
-        _: 'lodash',
+        AWS: "aws-sdk",
+        https: "https",
+        _: "lodash"
       },
       usage: `AWS.config.update(_.merge({
   maxRetries: 8,
@@ -29,63 +29,61 @@ module.exports = [
     agent: new https.Agent({
       keepAlive: false,
       rejectUnauthorized: true,
-      secureProtocol: 'TLSv1_method',
-      ciphers: 'ALL',
+      secureProtocol: "TLSv1_method",
+      ciphers: "ALL",
     }),
   },
 }, configFile.aws));`
     },
-    files: [
-      'services/userStorage.js'
-    ],
+    files: ["services/userStorage.js"],
     env: [
       {
-        'dynamoDB' : {
-          'tables' : {
-            'users' : ''
+        dynamoDB: {
+          tables: {
+            users: ""
           }
         }
       },
       {
-        'aws' : {
-          'region' : 'us-east-1'
+        aws: {
+          region: "us-east-1"
         }
       }
     ],
     requires: {
-      AWS: 'aws-sdk',
-      voxa: 'voxa',
-      UserStorage: '../services/userStorage',
+      AWS: "aws-sdk",
+      voxa: "voxa",
+      UserStorage: "../services/userStorage"
     },
     usage: `  const adapter = new UserStorage();
   voxa.plugins.autoLoad(app, { adapter });`
   },
   {
-    name: 'state-flow',
-    usage: '  voxa.plugins.stateFlow(app);'
+    name: "state-flow",
+    usage: "  voxa.plugins.stateFlow(app);"
   },
   {
-    name: 'cloud-watch',
+    name: "cloud-watch",
     dependencies: [
       {
-        name: 'aws-sdk',
-        version: '^2.7.23'
+        name: "aws-sdk",
+        version: "^2.7.23"
       }
     ],
     env: [
       {
-        'aws' : {
-          'region' : 'us-east-1'
+        aws: {
+          region: "us-east-1"
         }
       }
     ],
     requires: {
-      AWS: 'aws-sdk',
+      AWS: "aws-sdk"
     },
     usage: `  const cloudWatch = new AWS.CloudWatch({});
   const eventMetric = {
-    MetricName: 'Caught Error', // Name of your metric
-    Namespace: 'appName' // Name of your app
+    MetricName: "Caught Error", // Name of your metric
+    Namespace: "appName" // Name of your app
   };
 
   voxa.plugins.cloudWatch(app, cloudWatch, eventMetric);`
