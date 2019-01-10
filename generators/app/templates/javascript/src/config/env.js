4ed3aa38d13863f4c1f2ca39bf1e6bb2f87d6fc1
Christian Torres <chtorrez@gmail.com>
2019-01-10 15:55:09 -0600

    More linting changes.

--- generators/app/templates/javascript/src/config/env.js
+++ generators/app/templates/javascript/src/config/env.js
@@ -7,10 +7,12 @@ function getEnv() {
 
   // Environment base on lambda function name
   // TODO: put your own lambda function name(s) here
-  const AWS_LAMBDA_PRODUCTION_ENV = ['']; // production
+  const AWS_LAMBDA_PRODUCTION_ENV = [""]; // production
   const AWS_LAMBDA_FUNCTION_NAME = process.env.AWS_LAMBDA_FUNCTION_NAME;
   if (AWS_LAMBDA_FUNCTION_NAME) {
-    return AWS_LAMBDA_PRODUCTION_ENV.includes(AWS_LAMBDA_FUNCTION_NAME) ? "production" : "staging";
+    return AWS_LAMBDA_PRODUCTION_ENV.includes(AWS_LAMBDA_FUNCTION_NAME)
+      ? "production"
+      : "staging";
   }
 
   return "local";
--- generators/app/templates/typescript/src/config/env.ts
+++ generators/app/templates/typescript/src/config/env.ts
@@ -5,10 +5,12 @@ function getEnv() {
 
   // Environment base on lambda function name
   // TODO: put your own lambda function name(s) here
-  const AWS_LAMBDA_PRODUCTION_ENV: Array<string> = ['']; // production
+  const AWS_LAMBDA_PRODUCTION_ENV: Array<string> = [""]; // production
   const AWS_LAMBDA_FUNCTION_NAME = process.env.AWS_LAMBDA_FUNCTION_NAME;
   if (AWS_LAMBDA_FUNCTION_NAME) {
-    return AWS_LAMBDA_PRODUCTION_ENV.indexOf(AWS_LAMBDA_FUNCTION_NAME) !== -1 ? "production" : "staging";
+    return AWS_LAMBDA_PRODUCTION_ENV.indexOf(AWS_LAMBDA_FUNCTION_NAME) !== -1
+      ? "production"
+      : "staging";
   }
 
   return "local";
