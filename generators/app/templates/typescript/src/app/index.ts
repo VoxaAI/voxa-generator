0590f7d34034f34ae5f838a9e4524161a30fa904
Christian Torres <chtorrez@gmail.com>
2019-01-22 16:32:16 -0600

    Fixing issue with lodash types and improving imports in src/app/index.ts

--- generators/app/templates/typescript/src/app/index.ts
+++ generators/app/templates/typescript/src/app/index.ts
@@ -1,23 +1,23 @@
 import * as Raven from "raven"; // Official `raven` module
 import * as RavenLambdaWrapper from "serverless-sentry-lib";
-import * as voxa from "voxa";
+import { VoxaApp, AlexaPlatform, DialogFlowPlatform as DialogflowPlatform } from "voxa";
 import * as main from "./main";
 import * as alexaStates from "./states/alexa.states";
 import * as dialogflowStates from "./states/dialogflow.states";
 import * as variables from "./variables";
 import views from "./views";
 
-const app = new voxa.VoxaApp({ variables, views });
+const app = new VoxaApp({ variables, views });
 
 main.register(app);
 
 // alexa
-export const alexaSkill = new voxa.AlexaPlatform(app);
+export const alexaSkill = new AlexaPlatform(app);
 alexaStates.register(alexaSkill);
 export const alexa = RavenLambdaWrapper.handler(Raven, alexaSkill.lambda());
 
 // dialogflow
-export const dialogflowAction = new voxa.DialogFlowPlatform(app);
+export const dialogflowAction = new DialogflowPlatform(app);
 dialogflowStates.register(dialogflowAction);
 export const dialogflow = RavenLambdaWrapper.handler(
   Raven,
